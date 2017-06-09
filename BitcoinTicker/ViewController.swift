95e00fd7f44358446f9610d9c7b97c9520e1742e
Haakon W Hoel Bakker <haakonbakker@haakons-macbook-pro.local>
2017-06-09 18:28:09 +0200

    Initial Commit

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Haakon W Hoel Bakker on 09/06/2017.
+//  Copyright © 2017 Haakon W Hoel Bakker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
